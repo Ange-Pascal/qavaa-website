337b54e3d26dbd4e25c8dea788654a78b88e54e8
Ange-Pascal <angeakoumoua@gmail.com>
2023-07-13 15:07:39 +0200

    first commit

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -25,12 +25,12 @@
 
                 <ul class="timeline">
                     <li>
-                        <div class="timeline-image"><img class="rounded-circle img-fluid" src="assets/img/about/1.jpg"
+                        <div class="timeline-image"><img class="rounded-circle img-fluid" src="assets/img/about/value1.jpg"
                                 alt="..." /></div>
                         <div class="timeline-panel">
                             <div class="timeline-heading">
                                 <h4 class="priority" style="color: grey">Our Priority</h4>
-                                <h4 class="subheading">Our Values</h4>
+                                <h4 class="subheading" style="font-weight: bold">Our Values</h4>
                             </div>
                             <div class="timeline-body">
                                 <p class="text-muted">We work tirelessly towards delighting our customers with exceptional
@@ -40,12 +40,12 @@
                         </div>
                     </li>
                     <li class="timeline-inverted">
-                        <div class="timeline-image"><img class="rounded-circle img-fluid" src="assets/img/about/2.jpg"
+                        <div class="timeline-image"><img class="rounded-circle img-fluid" src="assets/img/about/value1.jpg"
                                 alt="..." /></div>
                         <div class="timeline-panel">
                             <div class="timeline-heading">
                                 <h4 class="priority" style="color: grey">Our Priority</h4>
-                                <h4 class="subheading">Our Mission</h4>
+                                <h4 class="subheading" style="font-weight: bold">Our Mission</h4>
                             </div>
                             <div class="timeline-body">
                                 <p class="text-muted">Our mission is to bring transparency and certainty in the business
@@ -56,12 +56,12 @@
                         </div>
                     </li>
                     <li>
-                        <div class="timeline-image"><img class="rounded-circle img-fluid" src="assets/img/about/3.jpg"
+                        <div class="timeline-image"><img class="rounded-circle img-fluid" src="assets/img/about/value1.jpg"
                                 alt="..." /></div>
                         <div class="timeline-panel">
                             <div class="timeline-heading">
-                                <h4>December 2015</h4>
-                                <h4 class="subheading">Transition to Full Service</h4>
+                                <h4 class="priority" style="color: grey">Our Vision</h4>
+                                <h4 class="subheading" style="font-weight: bold">Our Vision</h4>
                             </div>
                             <div class="timeline-body">
                                 <p class="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt ut
@@ -71,12 +71,12 @@
                         </div>
                     </li>
                     <li class="timeline-inverted">
-                        <div class="timeline-image"><img class="rounded-circle img-fluid" src="assets/img/about/4.jpg"
+                        <div class="timeline-image"><img class="rounded-circle img-fluid" src="assets/img/about/value1.jpg"
                                 alt="..." /></div>
                         <div class="timeline-panel">
                             <div class="timeline-heading">
-                                <h4>July 2020</h4>
-                                <h4 class="subheading">Phase Two Expansion</h4>
+                                <h4 class="priority" style="color: grey">Lorem ipsum</h4>
+                                <h4 class="subheading" style="font-weight: bold">Lorem ipsum dolor</h4>
                             </div>
                             <div class="timeline-body">
                                 <p class="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt ut
@@ -98,6 +98,93 @@
                     </li>
                 </ul>
             </div>
-        </section>
+        </section>  
+
+        <!-- Faq Section - Home Page -->
+    <section id="faq" class="faq">
+
+        <div class="container">
+
+            <div class="row gy-4">
+
+                <div class="col-lg-4" data-aos="fade-up" data-aos-delay="100">
+                    <div class="content px-xl-5">
+                        <h3><span>Frequently Asked </span><strong>Questions</strong></h3>
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                            labore et dolore magna aliqua. Duis aute irure dolor in reprehenderit
+                        </p>
+                    </div>
+                </div>
+
+                <div class="col-lg-8" data-aos="fade-up" data-aos-delay="200">
+
+                    <div class="faq-container">
+                        <div class="faq-item faq-active">
+                            <h3><span class="num">1.</span> <span>Non consectetur a erat nam at lectus urna duis?</span>
+                            </h3>
+                            <div class="faq-content">
+                                <p>Feugiat pretium nibh ipsum consequat. Tempus iaculis urna id volutpat lacus laoreet non
+                                    curabitur gravida. Venenatis lectus magna fringilla urna porttitor rhoncus dolor purus
+                                    non.</p>
+                            </div>
+                            <i class="faq-toggle bi bi-chevron-right"></i>
+                        </div><!-- End Faq item-->
+
+                        <div class="faq-item">
+                            <h3><span class="num">2.</span> <span>Feugiat scelerisque varius morbi enim nunc faucibus a
+                                    pellentesque?</span></h3>
+                            <div class="faq-content">
+                                <p>Dolor sit amet consectetur adipiscing elit pellentesque habitant morbi. Id interdum velit
+                                    laoreet id donec ultrices. Fringilla phasellus faucibus scelerisque eleifend donec
+                                    pretium. Est pellentesque elit ullamcorper dignissim. Mauris ultrices eros in cursus
+                                    turpis massa tincidunt dui.</p>
+                            </div>
+                            <i class="faq-toggle bi bi-chevron-right"></i>
+                        </div><!-- End Faq item-->
+
+                        <div class="faq-item">
+                            <h3><span class="num">3.</span> <span>Dolor sit amet consectetur adipiscing elit
+                                    pellentesque?</span></h3>
+                            <div class="faq-content">
+                                <p>Eleifend mi in nulla posuere sollicitudin aliquam ultrices sagittis orci. Faucibus
+                                    pulvinar elementum integer enim. Sem nulla pharetra diam sit amet nisl suscipit. Rutrum
+                                    tellus pellentesque eu tincidunt. Lectus urna duis convallis convallis tellus. Urna
+                                    molestie at elementum eu facilisis sed odio morbi quis</p>
+                            </div>
+                            <i class="faq-toggle bi bi-chevron-right"></i>
+                        </div><!-- End Faq item-->
+
+                        <div class="faq-item">
+                            <h3><span class="num">4.</span> <span>Ac odio tempor orci dapibus. Aliquam eleifend mi in
+                                    nulla?</span></h3>
+                            <div class="faq-content">
+                                <p>Dolor sit amet consectetur adipiscing elit pellentesque habitant morbi. Id interdum velit
+                                    laoreet id donec ultrices. Fringilla phasellus faucibus scelerisque eleifend donec
+                                    pretium. Est pellentesque elit ullamcorper dignissim. Mauris ultrices eros in cursus
+                                    turpis massa tincidunt dui.</p>
+                            </div>
+                            <i class="faq-toggle bi bi-chevron-right"></i>
+                        </div><!-- End Faq item-->
+
+                        <div class="faq-item">
+                            <h3><span class="num">5.</span> <span>Tempus quam pellentesque nec nam aliquam sem et tortor
+                                    consequat?</span></h3>
+                            <div class="faq-content">
+                                <p>Molestie a iaculis at erat pellentesque adipiscing commodo. Dignissim suspendisse in est
+                                    ante in. Nunc vel risus commodo viverra maecenas accumsan. Sit amet nisl suscipit
+                                    adipiscing bibendum est. Purus gravida quis blandit turpis cursus in</p>
+                            </div>
+                            <i class="faq-toggle bi bi-chevron-right"></i>
+                        </div><!-- End Faq item-->
+
+                    </div>
+
+                </div>
+            </div>
+
+        </div>
+
+    </section><!-- End Faq Section -->
     </div>
 @endsection
